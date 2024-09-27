fff642b8f46d83777f52540dee6f778a5d167d6c
Kactuswow <bryanalvarezg.kcts@gmail.com>
2024-09-27 18:32:28 -0500

    pg-connection 192096

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { environment } from './environment.js'
+import { PgConnection } from '../services/pgConnection.services.js'
 
 export class Server {
   constructor () {
@@ -7,7 +8,12 @@ export class Server {
     this.port = environment.port
   }
 
+  async connectionDb () {
+    new PgConnection()
+  }
+
   runServer () {
+    this.connectionDb()
     this.app.listen(this.port, () => {
       console.log(`Server is running on http://localhost:${this.port}`)
     })
