5040c445def3be8943d7546df51d280e2b45fefd
Yademir Hernandez <yhernandezs@ufpso.edu.co>
2024-09-27 21:07:43 -0500

    Update bd, models y controllers 192100

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,10 +1,18 @@
-import { getProductsModel } from '../models/product.models.js'
+import { getProductOneModel, getProductsModel } from '../models/product.models.js'
 
-export function getAllProducts (req, res) {
-  const datosProducts = getProductsModel()
+export async function getAllProducts (req, res) {
+  const dataProducts = await getProductsModel()
   res.status(200).json({
     success: true,
-    msg: 'llegamos al getAllProduct (controller)',
-    data: datosProducts
+    data: dataProducts
+  })
+}
+
+export async function getProductOne (req, res) {
+  const { idProduct } = req.params
+  const dataProduct = await getProductOneModel(idProduct)
+  res.status(200).json({
+    success: true,
+    data: dataProduct
   })
 }
--- src/models/product.models.js
+++ src/models/product.models.js
@@ -1,3 +1,20 @@
-export const getProductsModel = () => {
-  return 'estamos bien'
+import { PgConnection } from '../services/pgConnection.services.js'
+
+export const getProductsModel = async () => {
+  const pg = new PgConnection()
+  return await pg.connection.query('SELECT * FROM PRODUCT')
+}
+
+export const getProductOneModel = async (idProduct) => {
+  try {
+    const pg = new PgConnection()
+    return await pg.connection.query('SELECT * FROM PRODUCT WHERE ID_PRODUCT = $1', [idProduct])
+  } catch (error) {
+    return []
+  }
+}
+
+export const postProductModel = async (name, description, price) => {
+  const pg = new PgConnection()
+  return await pg.connection.query('INSERT INTO PRODUCT (NAME_PRODUCT, DESCRIPTION_PRODUCT, PRICE_PRODUCT) VALUES ($1, $2, $3) returning *', [name, description, price])
 }
