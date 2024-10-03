6d2e9cb79b09fb88b294bcd54bccfe3513db75cc
Yademir Hernandez <yhernandezs@ufpso.edu.co>
2024-10-02 21:47:42 -0500

    Solucion error en bd y coneccion 192100

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,4 +1,4 @@
-import { getProductOneModel, getProductsModel } from '../models/product.models.js'
+import { getProductOneModel, getProductsModel, postProductModel } from '../models/product.models.js'
 
 export async function getAllProducts (req, res) {
   const dataProducts = await getProductsModel()
@@ -14,5 +14,15 @@ export async function getProductOne (req, res) {
   res.status(200).json({
     success: true,
     data: dataProduct
+  })
+}
+
+export const postProduct = async (req, res) => {
+  const { name, description, price } = req.body
+  const data = await postProductModel(name, description, price)
+  res.status(200).json({
+    success: true,
+    msg: 'Llegamos al postCar',
+    data
   })
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getAllProducts } from '../controllers/product.controllers.js'
+import { getAllProducts, getProductOne } from '../controllers/product.controllers.js'
 
 export const productRoutes = Router()
 
 productRoutes.get('/', getAllProducts)
-productRoutes.post('/')
+productRoutes.get('/:idProduct', getProductOne)
+productRoutes.post('/', getProductOne)
 productRoutes.put('/')
 productRoutes.delete('/')
